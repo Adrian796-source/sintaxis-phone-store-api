5a8aee292854010f995552612fef600ec61f7dee
Adrian796-source <adrianbastaloguzzo@gmail.com>
2026-09-04 10:48:14 -0300

    refactor: Eliminar public en ProductoServiceTest para cumplir S5786

--- src/test/java/com/adrian/sintaxis/service/ProductoServiceTest.java
+++ src/test/java/com/adrian/sintaxis/service/ProductoServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductoServiceTest {
+class ProductoServiceTest {
 
     @Mock
     private ProductoRepository productoRepository;
